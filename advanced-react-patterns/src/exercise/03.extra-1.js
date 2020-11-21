2d9cc912d8cdb96de83c92930a37dd8bceba5b00
Juan Petterson Heberle <juan.petterson@gmail.com>
2020-11-21 00:28:01 -0300

    add flexible compound components with context api exercise

---
+++ advanced-react-patterns/src/exercise/03.extra-1.js
@@ -0,0 +1,54 @@
+// Flexible Compound Components
+// http://localhost:3000/isolated/exercise/03.js
+
+import * as React from 'react'
+import {Switch} from '../switch'
+
+const ToggleContext = React.createContext()
+ToggleContext.displayName = 'ToggleContext'
+
+function Toggle({onToggle, children}) {
+  const [on, setOn] = React.useState(false)
+  const toggle = () => setOn(!on)
+
+  return (
+    <ToggleContext.Provider value={{on, toggle}}>
+      {children}
+    </ToggleContext.Provider>
+  )
+}
+
+function useToggle() {
+  const context = React.useContext(ToggleContext)
+
+  if (!context) {
+    throw new Error('useToggle must be used within a <Toggle/>.')
+  }
+
+  return context
+}
+
+function ToggleOn({children}) {
+  const {on} = useToggle()
+  return on ? children : null
+}
+
+function ToggleOff({children}) {
+  const {on} = useToggle()
+  return on ? null : children
+}
+
+function ToggleButton(props) {
+  const {on, toggle} = useToggle()
+
+  return <Switch on={on} onClick={toggle} {...props} />
+}
+
+const App = () => <ToggleButton />
+
+export default App
+
+/*
+eslint
+  no-unused-vars: "off",
+*/
